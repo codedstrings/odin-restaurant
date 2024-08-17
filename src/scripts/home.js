de93a232ae7614af76f19e53ed868a2a58e0fe43
codedstrings <mridhulml@gmail.com>
2024-08-17 08:45:00 +0530

    change home content-test

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -4,16 +4,12 @@ export function loadHome() {
     content.innerHTML = '';
 
     const h1 = document.createElement('h1');
-    h1.textContent = 'Welcome to Our Restaurant';
-
-    const bgImage = new Image();
-    bgImage.src = Background;
+    h1.textContent = 'Welcome Aboard The Thai-Tanic!';
 
 
     const p = document.createElement('p');
-    p.textContent = 'Experience the finest cuisine in town at our cozy and elegant restaurant.';
+    p.textContent = 'Embark on a Culinary Voyage!';
 
     content.appendChild(h1);
-    // content.appendChild(bgImage);
     content.appendChild(p);
 }
